9a15cddc31d806c9835dd3f604e176cf592093cd
JMiguelDB <jose.miguel.db.17894@gmail.com>
2017-02-02 17:24:06 +0100

    Implementada nueva funcionalidad

---
+++ image-processing/basic-processing/process-images.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 19:56:30 2017
+
+@author: JM
+"""
+import cv2
+import numpy as np
+
+def meanSquareError(imagenOriginal,imagenRestaurada):
+    M, N = imagenOriginal.shape[:2]
+    error = np.float32(0.0)
+    for x  in range(M):
+        for y in range(N):
+            error += ((imagenRestaurada[x][y]-imagenOriginal[x][y])**2)
+    error = np.sqrt((1/(M*N))*error)
+    return error
+    
+def noisy(image):
+      #gaussian
+      aux = image.copy()
+      aux = cv2.randn(aux,aux.mean(),aux.std())
+      gaussian = cv2.add(image, aux)
+      #salt and pepper
+      s_vs_p = 0.5
+      amount = 0.004
+      out = np.copy(image)
+      # Salt mode
+      num_salt = np.ceil(amount * image.size * s_vs_p)
+      coords = [np.random.randint(0, i - 1, int(num_salt))
+              for i in image.shape]
+      out[coords] = 1
+
+      # Pepper mode
+      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+      coords = [np.random.randint(0, i - 1, int(num_pepper))
+              for i in image.shape]
+      out[coords] = 0
+      return gaussian,out
+      
+img = cv2.imread('../images/prueba.jpg',0)
+img = cv2.resize(img,(600,500), interpolation = cv2.INTER_CUBIC)
+gaussian,salt = noisy(img)
+
+gaussianblur1 = cv2.GaussianBlur(img,(3,3),0)
+gaussianblur2 = cv2.GaussianBlur(img,(5,5),0)
+gaussianblur3 = cv2.GaussianBlur(img,(7,7),0)
+
+median1 = cv2.medianBlur(salt,3)
+median2 = cv2.medianBlur(salt,5)
+median3 = cv2.medianBlur(salt,7)
+
+
+cv2.imshow("Original",img)
+cv2.imshow("salt and pepper",salt)
+cv2.waitKey(0)
+cv2.imshow("salt and pepper",median1)
+cv2.waitKey(0)
+cv2.imshow("salt and pepper",median2)
+cv2.waitKey(0)
+cv2.imshow("salt and pepper",median3)
+cv2.waitKey(0)
+equ = cv2.equalizeHist(median1)
+cv2.imshow("salt and pepper",equ)
+cv2.waitKey(0)
+cv2.imshow("gaussian",gaussian)
+cv2.waitKey(0)
+cv2.imshow("gaussian",gaussianblur1)
+cv2.waitKey(0)
+cv2.imshow("gaussian",gaussianblur2)
+cv2.waitKey(0)
+cv2.imshow("gaussian",gaussianblur3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print('Error s&p 1:',meanSquareError(img,median1))
+print('Error s&p 2:',meanSquareError(img,median2))
+print('Error s&p 3:',meanSquareError(img,median3))
+print('Error s&p 4:',meanSquareError(img,equ))
+
+print('Error gaussian 1:',meanSquareError(img,gaussianblur1))
+print('Error gaussian 2:',meanSquareError(img,gaussianblur2))
+print('Error gaussian 3:',meanSquareError(img,gaussianblur3))

9348bb431bf16b77736b6e41aba11b31c3c6f7c5
José Miguel <jose.miguel.db.17894@gmail.com>
2017-02-20 17:22:37 +0100

    Añadido ejercicio de deteccion de codigo de barras

---
+++ image-processing/barcode-detecting/barcode-detecting.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+
+imagen = 'barcode1.jpg'
+
+
+# load the image and convert it to grayscale
+image = cv2.imread('../images/' + imagen)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+ 
+# compute the Scharr gradient magnitude representation of the images
+# in both the x and y direction
+gradX = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
+gradY = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = -1)
+ 
+# subtract the y-gradient from the x-gradient
+#Se le aplica una resta para resaltar el resultado del eje X(Codigo de barras) frente al eje Y(numeros del codigo)
+gradient = cv2.subtract(gradX, gradY)
+gradient = cv2.convertScaleAbs(gradient)
+
+# blur and threshold the image
+blurred = cv2.blur(gradient, (5, 5))
+bilateralFilter = cv2.bilateralFilter(gradient,9,75,75)
+(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+(_, thresh1) = cv2.threshold(bilateralFilter, 225, 255, cv2.THRESH_BINARY)
+
+# construct a closing kernel and apply it to the thresholded image
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+closed = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, kernel)
+
+# perform a series of erosions and dilations
+closed = cv2.erode(closed, None, iterations = 4)
+closed = cv2.dilate(closed, None, iterations = 4)
+
+# find the contours in the thresholded image, then sort the contours
+# by their area, keeping only the largest one
+(_,cnts ,_) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+ 
+# compute the rotated bounding box of the largest contour
+rect = cv2.minAreaRect(c)
+box = np.int0(cv2.boxPoints(rect))
+ 
+# draw a bounding box arounded the detected barcode and display the
+# image
+cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+
+cv2.imshow('X',gradX)
+cv2.imshow('Y',gradY)
+cv2.imshow('',closed)
+cv2.imshow('Blurred',blurred)
+cv2.imshow('Threshold',thresh)
+cv2.imshow('Bilateral',bilateralFilter)
+cv2.imshow('Threshold 1',thresh1)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+

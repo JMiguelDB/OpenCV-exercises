0a564985316bcb4be5f70ceb6026b632e9f2ecf6
José Miguel <jose.miguel.db.17894@gmail.com>
2017-02-23 20:31:08 +0100

    implementada deteccion de codigo de barras

--- image-processing/barcode-detecting/barcode-detecting.py
+++ image-processing/barcode-detecting/barcode-detecting.py
@@ -4,78 +4,94 @@ import numpy as np
 import cv2
 
 
-def align_boundary(img, x, y, begin, end):
-    if (img[y][x] == end):
-        while (img[y][x] == end):
-            x += 1
+def align_boundary(img, cursor, begin, end):
+    if (img[cursor[1]][cursor[0]] == end):
+        while (img[cursor[1]][cursor[0]] == end):
+            cursor[0] += 1
     else:
-        while (img[y][x - 1] == begin):
-            x -= 1;
-    return x
+        while (img[cursor[1]][cursor[0]-1] == begin):
+            cursor[0] -= 1;
       
-def read_digit(img, x, y, unit_width, Lcode, Rcode, position, SPACE, BAR):
-
+def read_digit(img, cursor, unit_width, Lcode, Gcode,Rcode, position, SPACE, BAR):
   # Read the 7 consecutive bits.
   pattern = [0, 0, 0, 0, 0, 0, 0]
   for i in range(len(pattern)):
     for j in range(unit_width):
-      if (img[y][x] == 255):
+      if (img[cursor[1]][cursor[0]] == BAR):
         pattern[i] += 1
-      x += 1
+      cursor[0] += 1
 
-    if ((pattern[i] == 1 and img[y,x] == BAR) or (pattern[i] == unit_width-1 and img[y,x] == SPACE)):
-      x -= 1
+    if (pattern[i] == 1 and img[cursor[1]][cursor[0]] == BAR 
+        or pattern[i] == unit_width-1 and img[cursor[1]][cursor[0]] == SPACE):
+      cursor[0] -= 1
  
   # Convert to binary, consider that a bit is set if the number of
   # bars encountered is greater than a threshold.
   threshold = unit_width / 2
   v = ""
   for i in range(len(pattern)):
-    v = (v << 1) + (pattern[i] >= threshold)
- 
+    if pattern[i] >= threshold:
+        v += str(1)
+    else:
+        v += str(0)
+  
+  encoding=""
   # Lookup digit value.
   if (position == "LEFT"):
     digit = Lcode.get(v)
-    x = align_boundary(img, x, y, SPACE, BAR)
+    encoding = "L"
+    if digit is None:
+       digit = Gcode.get(v)
+       encoding = "G"
+    align_boundary(img, cursor, SPACE, BAR)
   else:
-    # Bitwise complement (only on the first 7 bits).
     digit = Rcode.get(v)
-    x = align_boundary(img, x, y, BAR, SPACE) 
-  #display(img, cur);
-  return digit
+    align_boundary(img, cursor, BAR, SPACE) 
+  print("El codigo vale", v, "Digito", digit)
+  return digit,encoding
   
-def skip_quiet_zone(img, x, y, SPACE):
-  print(img[y][x])
-  while (img[y][x] == SPACE):
-      x+=1
-  return x
+def skip_quiet_zone(img, cursor, SPACE):
+  while (img[cursor[1]][cursor[0]] == SPACE):
+      cursor[0]+=1
   
-def read_lguard(img, x, y, BAR, SPACE):
+def read_lguard(img, cursor, BAR, SPACE):
   widths = [ 0, 0, 0 ]
   pattern = [ BAR, SPACE, BAR ]
   for i in range(len(widths)):
-    while (img[y][x] == pattern[i]):
-      x+=1
+    while (img[cursor[1]][cursor[0]] == pattern[i]):
+      cursor[0]+=1
       widths[i]+=1
   return widths[0];
 
-def skip_mguard(img, x, y, BAR, SPACE):
+def skip_mguard(img, cursor, BAR, SPACE):
   pattern = [ SPACE, BAR, SPACE, BAR, SPACE ]
   for i in range(len(pattern)):
-    while (img[y][x] == pattern[i]):
-      x+=1
-  return x
+    while (img[cursor[1]][cursor[0]] == pattern[i]):
+      cursor[0]+=1
+
 def read_barcode(filename):
   #Definimos todas las variables necesarias 
   img = cv2.imread('../images/' + filename, 0)
-  x = 0
-  y = int(len(img[0]) / 2)
+  img = cv2.bitwise_not(img)
+  _,img = cv2.threshold(img,128,255,cv2.THRESH_BINARY)
+  cv2.imshow("original",img)
+  #img = cv2.threshold(img, 128,255, cv2.THRESH_BINARY)
+  
+  #Definimos una lista como el cursor con los ejes [X,Y]
+  cursor = [0,int(len(img[0]) / 2)]
+
   #Se definen las dos regiones que se encuentran dentro del codigo de barras
   Lcode = {"0001101":0,"0011001":1,"0010011":2,"0111101":3,"0100011":4,
              "0110001":5,"0101111":6,"0111011":7,"0110111":8,"0001011":9}
+  
+  Gcode = {"0100111":0,"0110011":1,"0011011":2,"0100001":3,"0011101":4,
+             "0111001":5,"0000101":6,"0010001":7,"0001001":8,"0010111":9}          
              
   Rcode = {"1110010":0,"1100110":1,"1101100":2,"1000010":3,"1011100":4,
              "1001110":5,"1010000":6,"1000100":7,"1001000":8,"1110100":9}
+             
+  typeEncoding = {"LLLLLL":0,"LLGLGG":1,"LLGGLG":2,"LLGGGL":3,"LGLLGG":4,
+             "LGGLLG":5,"LGGGLL":6,"LGLGLG":7,"LGLGGL":8,"LGGLGL":9}
   #Definimos los espacios en blanco y las barras negras
   SPACE = 0
   BAR = 255
@@ -84,35 +100,33 @@ def read_barcode(filename):
   #cv::bitwise_not(img, img);
   #cv::threshold(img, img, 128, 255, cv::THRESH_BINARY);
   ## ------------------------------
-  
   #Nos saltamos la primera zona de seguridad
-  x = skip_quiet_zone(img, x, y, SPACE)
-  print("pasa de aqui")
+  skip_quiet_zone(img,cursor, SPACE)
   #Calibramos el valor para que se coloque en la primera barra del codigo izquierdo
-  unit_width = read_lguard(img, x, y, BAR, SPACE)
-
+  unit_width = read_lguard(img, cursor, BAR, SPACE)
   #Almacenamos los digitos asociados al valor del codigo izquierdo
   #std::vector<int> digits;
   digits = ""
+  encoding = ""
   for i in range(6):
-    d = read_digit(img, x, y, unit_width, Lcode, Rcode, "LEFT", SPACE, BAR)   
-    digits += d
+    d,e = read_digit(img, cursor, unit_width, Lcode, Gcode, Rcode, "LEFT", SPACE, BAR)   
+    digits += str(d)
+    encoding += e
   
   #Saltamos las barras de seguridad del medio
-  skip_mguard(img, x, y, BAR, SPACE)
+  skip_mguard(img, cursor, BAR, SPACE)
  
   #Almacenamos los digitos asociados al valor del codigo derecho
   for i in range(6):
-    d = read_digit(img, x, y, unit_width, Lcode, Rcode, "RIGHT", SPACE, BAR)
-    digits += d
+    d,_ = read_digit(img, cursor, unit_width, Lcode, Gcode, Rcode, "RIGHT", SPACE, BAR)
+    digits += str(d)
 
- 
-  print(digits)
-   
-    
+  firstDigit = typeEncoding.get(encoding)
+  print(str(firstDigit)+digits)
 
-#imagen = 'barcode1.jpg'
-imagen = 'barcode.png'
+#imagen = 'barcodes/20170223_151821.jpg'
+imagen = 'barcode5.jpg'
+#imagen = 'barcode.png' ##Imagen wena pa k funke
 read_barcode(imagen)
 
 """
@@ -168,8 +182,8 @@ cv2.imshow('Blurred',blurred)
 cv2.imshow('Threshold',thresh)
 cv2.imshow('Bilateral',bilateralFilter)
 cv2.imshow('Threshold 1',thresh1)
+"""
+
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
-
-"""

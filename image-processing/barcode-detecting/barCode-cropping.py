d8b58c04853071a9775b609f21703a91d6cd7808
Jose Miguel <jose.miguel.db.17894@gmail.com>
2017-03-01 14:42:18 +0100

    Añadida nuevo algoritmo de decodificacion

--- image-processing/barcode-detecting/barCode-cropping.py
+++ image-processing/barcode-detecting/barCode-cropping.py
@@ -7,8 +7,8 @@ import os
 nombre = os.popen('ls fotos/').read()
 
 def nombres(nombre):
-	datos =nombre.split("\n")
-	return(datos[:len(datos)-1])
+    datos =nombre.split("\n")
+    return(datos[:len(datos)-1])
 nom = nombres(nombre)
 
 #print (nom)
@@ -16,93 +16,100 @@ nom = nombres(nombre)
 ## este metodo se usa para recortar el codigo de barras
 ## el margen derecho es muy corto, ¿posibles problemas?
 def recorta(box):
-	x0 = box[0][0]
-	x1 = box[0][0]
-	y0 = box[0][0]
-	y1 = box[0][0]
-	for i in (range (len(box))):
-		x0 = min(x0 , box[i][0])
-		x1 = max(x1,  box[i][0])
-		y0 = min(y0 , box[i][1])
-		y1 = max(y1,  box[i][1])
+    x0 = box[0][0]
+    x1 = box[0][0]
+    y0 = box[0][0]
+    y1 = box[0][0]
+    for i in (range (len(box))):
+        x0 = min(x0 , box[i][0])
+        x1 = max(x1,  box[i][0])
+        y0 = min(y0 , box[i][1])
+        y1 = max(y1,  box[i][1])
 
-	return (x0,x1,y0,y1)
+    return (x0,x1,y0,y1)
 
 
 def barCode(name):
-	
-	image = cv2.imread("fotos/"+ name)
-	#print (name,len(image))
-	if(len(image)>683):
-		image = image = cv2.resize(image, (683, 384)) 		
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 	# Sobel se usa para buscar los bordes de la imagen
-	gradX = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
-	gradY = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = -1)
-	# al restar los valores y hacerle el valor absoulto tenemos una imagen de los filos
-	# de la imagen
-	gradient = cv2.subtract(gradX, gradY)
-	gradient = cv2.convertScaleAbs(gradient)
-	
-	# blur and threshold the image
-	blurred = cv2.bilateralFilter(gradient,9,75,75)
-	(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+    
+    image = cv2.imread('../images/'+ name)
+    #print (name,len(image))
+    """
+    if(len(image)>683):
+        image = image = cv2.resize(image, (683, 384)) 
+    """        
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+     # Sobel se usa para buscar los bordes de la imagen
+    gradX = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
+    gradY = cv2.Sobel(gray, ddepth = cv2.CV_32F, dx = 0, dy = 1, ksize = -1)
+    # al restar los valores y hacerle el valor absoulto tenemos una imagen de los filos
+    # de la imagen
+    gradient = cv2.subtract(gradX, gradY)
+    gradient = cv2.convertScaleAbs(gradient)
+    
+    # blur and threshold the image
+    blurred = cv2.bilateralFilter(gradient,9,75,75)
+    (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
 
-	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-	closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+    closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-	closed = cv2.erode(closed, None, iterations = 4)
-	closed = cv2.dilate(closed, None, iterations = 4)
-
-
-	cv2.imshow("",closed)
-	#cv2.waitKey()
-	# find the contours in the thresholded image, then sort the contours
-	# by their area, keeping only the largest one
-	(_,cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-	c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
-	 
-	# compute the rotated bounding box of the largest contour
-	rect = cv2.minAreaRect(c)
-	box = np.int0(cv2.boxPoints(rect))
-	# draw a bounding box arounded the detected barcode and display the
-	# image
-	#cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
-	#cv2.imshow("Image", image)
-	#cv2.waitKey(0)
-	x0,x1,y0,y1 =recorta(box)
-	#print(box)
-	cropped = image[ y0:y1,x0:x1]
-	if ((len (cropped)>0) and (len(cropped[0]))):
-		print(len(cropped),len(cropped[0]))
-		_,cropped = cv2.threshold(cropped,100,255,cv2.THRESH_BINARY)	
-		cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-		
-		cv2.imwrite("cropped+"+name, cropped)
-	#cv2.waitKey(0)
-	#recorre(cropped)
+    closed = cv2.erode(closed, None, iterations = 4)
+    closed = cv2.dilate(closed, None, iterations = 4)
 
 
+    cv2.imshow("",closed)
+    #cv2.waitKey()
+    # find the contours in the thresholded image, then sort the contours
+    # by their area, keeping only the largest one
+    (_,cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+    c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+     
+    # compute the rotated bounding box of the largest contour
+    rect = cv2.minAreaRect(c)
+    box = np.int0(cv2.boxPoints(rect))
+    # draw a bounding box arounded the detected barcode and display the
+    # image
+    #cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+    #cv2.imshow("Image", image)
+    #cv2.waitKey(0)
+    x0,x1,y0,y1 =recorta(box)
+    #print(box)
+    cropped = image[ y0:y1,x0:x1]
+    """
+    if ((len (cropped)>0) and (len(cropped[0]))):
+        print(len(cropped),len(cropped[0]))
+        _,cropped = cv2.threshold(cropped,100,255,cv2.THRESH_BINARY)    
+        cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+        
+        cv2.imwrite("cropped+"+name, cropped)
+    """
+    cv2.imshow("recortada", cropped)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #recorre(cropped)
+
+"""
 def decodifica():
 
-	valores = {"00000000":1}
-	#print(valores)
+    valores = {"00000000":1}
+    #print(valores)
 
 
 def recorre(image):
-	a = []
-	for i in range(len (image)):
-		a.append( (image[45][i]))
+    a = []
+    for i in range(len (image)):
+        a.append( (image[45][i]))
 
-	#print (a)
+    #print (a)
 
 for i in range(len(nom)):
-	print( i)	
-	barCode(nom[i])
+    print( i)    
+    barCode(nom[i])
 
-
+"""
+barCode("barcode-1.jpg")
 
 
 
---
+++ image-processing/barcode-detecting/main.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 28 10:54:40 2017
+
+@author: JM
+"""
+import cv2
+import numpy as np
+from barcodeDetecting import barcode
+
+def crop(box):
+    x0 = box[0][0]
+    x1 = box[0][0]
+    y0 = box[0][1]
+    y1 = box[0][1]
+    for i in (range (len(box))):
+        x0 = min(x0 , box[i][0])
+        x1 = max(x1,  box[i][0])
+        y0 = min(y0 , box[i][1])
+        y1 = max(y1,  box[i][1])
+
+    return (x0-5,x1+5,y0,y1)
+
+def readImage(filename):
+   image = cv2.imread('../images/' + filename)
+    
+   if(len(image)>1200):
+       image = image = cv2.resize(image, (1200, 800)) 
+        
+   return image
+
+def imageProcessing(original,image):
+    # construct a closing kernel and apply it to the thresholded image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+    closed = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
+    # perform a series of erosions and dilations
+    closed = cv2.erode(closed, None, iterations = 4)
+    closed = cv2.dilate(closed, None, iterations = 4)
+
+    # find the contours in the thresholded image, then sort the contours
+    # by their area, keeping only the largest one
+    (_,cnts ,_) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+    c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+ 
+    # compute the rotated bounding box of the largest contour
+    rect = cv2.minAreaRect(c)
+    box = np.int0(cv2.boxPoints(rect))
+    # draw a bounding box arounded the detected barcode and display the
+    # image
+    cv2.drawContours(original, [box], -1, (0, 255, 0), 3)
+    cv2.imshow("Image", original)
+    cv2.waitKey(0)
+    
+    return box
+    
+"""
+### -------------- Prueba 1: Barcode perfecto --------------------------------------------------
+#Leemos el fichero
+filename = "barcode.png"
+image = readImage(filename)
+
+#Convertimos la imagen a gris y le aplicamos la deteccion de vertices de Canny
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.Canny(gray,100,200)
+
+#Obtenemos el contorno del barcode y recortamos el barcode a partir del contorno
+box = imageProcessing(image.copy(),thresh)
+y1,y2,x1,x2 = crop(box)
+cropped = image[x1:x2,y1:y2]
+
+#Aplicamos la decodificacion del barcode
+barcode = barcode(cropped)
+barcode.read_barcode()
+barcode.show()
+###------------------------------------------------------------------------------------------
+"""
+###------------- Prueba 2: Barcode en objeto ---------------------------------------------
+#Leemos el fichero
+filename = "barcode-3.jpg"
+image = readImage(filename)
+
+#Rotamos la imagen 180º para obtener el barcode
+rot_mat = cv2.getRotationMatrix2D((image.shape[0]/2, image.shape[1]/2),180,1.0)
+image = cv2.warpAffine(image, rot_mat, (image.shape[0], image.shape[1]),flags=cv2.INTER_LINEAR)
+#Convertimos la imagen a gris y le aplicamos la deteccion de vertices de Canny
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.Canny(gray,50,100)
+
+#Obtenemos el contorno del barcode y recortamos el barcode a partir del contorno
+box = imageProcessing(image.copy(),thresh)
+y1,y2,x1,x2 = crop(box)
+cropped = image[x1:x2,y1:y2]
+
+
+#Aplicamos la decodificacion del barcode
+barcode = barcode(cropped)
+barcode.read_barcode()
+#barcode.show()
+###-------------------------------------------------------------------------------------
+
+
+
+
+cv2.destroyAllWindows()

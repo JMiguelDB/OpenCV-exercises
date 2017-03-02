304a67e4b836d89d74ec2120499b77a2abe7b416
Jose Miguel <jose.miguel.db.17894@gmail.com>
2017-03-02 14:51:06 +0100

    Añadida prueba con ruido, alejado y con poca iluminacion

--- image-processing/barcode-detecting/barcodeDetecting.py
+++ image-processing/barcode-detecting/barcodeDetecting.py
@@ -37,8 +37,10 @@ class barcode:
     #Metodo para el procesamiento del barcode para su posterior lectura de barras
     def __barcodeProcessing(self,image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
         blur = cv2.GaussianBlur(gray,(3,3),7)
         (_, thresh) = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        
         img = cv2.bitwise_not(thresh)
         
         return img
